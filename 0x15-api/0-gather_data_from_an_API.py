82df74de5e4bbdd8d301eebec16860bffcebc1fb
babajallow91 <momodousalieu.jallow@gmail.com>
2023-04-26 18:37:43 +0000

    py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,11 +5,11 @@ from sys import argv
 
 if __name__ == '__main__':
     # get employee response [used to get name in line 19]
-    endpoint = 'https://jsonplaceholder.typicode.com'
-    user_res = requests.get(endpoint + '/users/' + argv[1]).json()
+    url = 'https://jsonplaceholder.typicode.com'
+    user_res = requests.get(url + '/users/' + argv[1]).json()
 
     # get total number of tasks [used to get len of all tasks in line 18]
-    todos = requests.get(endpoint + '/todos?userId=' + argv[1]).json()
+    todos = requests.get(url + '/todos?userId=' + argv[1]).json()
 
     # get number completed tasks and their titles
     titles_done = [todo['title'] for todo in todos if todo['completed']]
