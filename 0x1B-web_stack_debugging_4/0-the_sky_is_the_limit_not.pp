f3b5429a0a2bb5de5f897451b1cf621fdd5e55ea
babajallow91 <momodousalieu.jallow@gmail.com>
2023-05-19 12:26:51 +0000

    bug

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,12 @@
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => "ULIMIT='-n 2048'\n",
+# Fix problem of high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
